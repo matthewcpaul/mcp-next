f3730bd369dbf0c156d78dcb31c425be5a2cde83
Matthew Traul <mclydep@gmail.com>
2025-06-15 16:39:30 -0600

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,102 +1,45 @@
 import Image from "next/image";
+import BubbleNativeMobileApps from '../../public/images/bubble-native-mobile-apps.png'
+import TheDotComAutomatedPages from '../../public/images/the-dot-com-automated-pages.png'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div className="max-w-[680px] mx-auto px-5 sm:px-8 pb-18 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col">
+        <header className="flex flex-col items-start sticky top-0 py-5 sm:py-6 bg-[var(--background)]">
+          <a className="font-semibold">Matthew Paul</a>
+        </header>
+        <article className="flex flex-col gap-16 mt-10">
+          <section className="flex flex-col">
+            <h1 className="font-semibold text-2xl">Software Product Designer</h1>
+            <p className="text-[var(--text-secondary)] mt-3">Product Designer and Design Engineer specializing in designer, developer, and collaboration tools.</p>
+            <p className="text-[var(--text-secondary)]">I have 12+ years of experience designing 0 – 1 products, shipping software at scale, and building and maintaining design systems.</p>
+          </section>
+          <section className="flex flex-col">
+            <h2 className="font-semibold text-2xl">Selected Work</h2>
+            <div className="flex flex-col mt-6">
+              <Image
+                src={BubbleNativeMobileApps}
+                alt="Thumbnail of Bubble Native Mobile Apps"
+                priority
+              />
+              <h3 className="font-semibold mt-5">Bubble Native Mobile Apps</h3>
+              <p className="text-[var(--text-secondary)]">Drove the direction, led the design, and executed on the details for the launch of the largest product expansion in Bubble’s history.</p>
+              <p className="text-[var(--text-secondary)]">I designed the core concepts for how to design, build, preview, and publish native mobile apps for iOS and Android in a newly redesigned no-code editor.</p>
+            </div>
+            <div className="flex flex-col mt-16">
+              <Image
+                src={TheDotComAutomatedPages}
+                alt="Thumbnail of The.com Page Generation"
+              />
+              <h3 className="font-semibold mt-5">The.com Page Generation</h3>
+              <p className="text-[var(--text-secondary)]">Led product, design, and design systems for The.com’s no-code, sheet-based website builder and CMS tooling.</p>
+              <p className="text-[var(--text-secondary)]">I learned from early customers through generative and evaluative research, iterated and improved on the core product, and designed a brand new product surface area called Page Generation.</p>
+            </div>
+          </section>
+        </article>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="flex gap-6 flex-wrap items-center justify-center">
       </footer>
     </div>
   );
