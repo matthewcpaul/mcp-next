a571a564c3a294b5f197ae50fa69fd096ad78478
Matthew Traul <mclydep@gmail.com>
2025-07-10 18:00:33 -0600

    Update lg styles

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import Avatar from '../../public/images/avatar.png'
 
 export default function Home() {
   return (
-    <div className="px-6 sm:px-12 md:px-20 pb-18 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col max-w-[64rem] mx-auto">
+    <div className="px-6 sm:px-12 lg:px-20 pb-18 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col max-w-[70rem] mx-auto">
         <header className="flex flex-col items-start sticky top-0 py-6 bg-[var(--background)]">
           <div className="flex items-center gap-3">
             <Image
@@ -15,41 +15,41 @@ export default function Home() {
               height={28}
               className="rounded-full"
             />
-            <a className="font-medium">Matthew Paul</a>
+            <span className="text-base font-medium">Matthew Paul</span>
           </div>
         </header>
-        <article className="flex flex-col gap-16 md:gap-24 mt-16 md:mt-30">
-          <section className="flex flex-col gap-6 md:gap-8">
-            <h1 className="text-4xl md:text-5xl md:leading-14 font-semibold">Product designer and design engineer specializing in designer, developer, and collaboration tools</h1>
-            <div className="grid grid-cols-1 mt-3">
-              <p className="max-w-[58rem] text-xl md:text-2xl md:leading-9 text-[var(--text-secondary)]">I have 12+ years of experience designing 0 → 1 products, shipping software at scale, and building and maintaining design systems.</p>
+        <article className="flex flex-col gap-16 lg:gap-28 mt-16 lg:mt-34">
+          <section className="flex flex-col gap-6 lg:gap-14">
+            <h1 className="text-2xl lg:text-3xl font-semibold tracking-tight">Product designer and design engineer specializing in designer, developer, and collaboration tools</h1>
+            <div className="grid grid-cols-1">
+              <p className="text-lg lg:text-xl text-[var(--text-secondary)]">I have 12+ years of experience designing 0 → 1 products, shipping software at scale, and building and maintaining design systems.</p>
             </div>
           </section>
           <section className="flex flex-col">
-            <h2 className="text-2xl md:text-3xl font-semibold">Selected Work</h2>
-            <div className="flex flex-col gap-6 md:gap-8 mt-6 md:mt-8">
+            <h2 className="text-lg lg:text-xl font-semibold tracking-tight">Selected Work</h2>
+            <div className="flex flex-col gap-6 lg:gap-8 mt-6 lg:mt-10">
               <video autoPlay loop muted preload="auto" playsInline>
-                <source src="/images/bubble-native-mobile-apps.mp4" type="video/mp4" />
+                <source src="/images/BubbleNativeMobileTabBar.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
               <div className="flex flex-col sm:flex-row gap-6 sm:gap-0">
-                <h3 className="flex-1 text-base md:text-lg font-semibold">Bubble Native Mobile Apps</h3>
-                <div className="flex flex-col gap-6 md:gap-7 flex-1">
-                  <p className="text-base md:text-lg text-[var(--text-secondary)]">Drove the direction, led the design, and executed on the details for the launch of the largest product expansion in Bubble’s history.</p>
-                  <p className="text-base md:text-lg text-[var(--text-secondary)]">I designed the core concepts for how to design, build, preview, and publish native mobile apps for iOS and Android in a redesigned no-code editor.</p>
+                <h3 className="flex-1 text-base lg:text-lg font-semibold">Bubble Native Mobile Apps</h3>
+                <div className="flex flex-col gap-6 lg:gap-7 flex-1">
+                  <p className="text-base lg:text-lg text-[var(--text-secondary)]">Drove the direction, led the design, and executed on the details for the launch of the largest product expansion in Bubble’s history.</p>
+                  <p className="text-base lg:text-lg text-[var(--text-secondary)]">I designed the core concepts for how to design, build, preview, and publish native mobile apps for iOS and Android in a redesigned no-code editor.</p>
                 </div>
               </div>
             </div>
-            <div className="flex flex-col gap-6 md:gap-8 mt-16 md:mt-24">
+            <div className="flex flex-col gap-6 lg:gap-8 mt-16 lg:mt-28">
               <Image
                 src={TheDotComAutomatedPages}
                 alt="Thumbnail of The.com Page Generation"
               />
               <div className="flex flex-col sm:flex-row gap-6 sm:gap-0">
-                <h3 className="flex-1 text-base md:text-lg font-semibold">The.com Page Generation</h3>
-                <div className="flex flex-col gap-6 md:gap-7 flex-1">
-                  <p className="text-base md:text-lg text-[var(--text-secondary)]">Led product, design, and design systems for The.com’s no-code, sheet-based website builder and CMS tools.</p>
-                  <p className="text-base md:text-lg text-[var(--text-secondary)]">I learned from early customers through generative and evaluative research, iterated and improved on the core product, and designed a brand new product surface area called Page Generation.</p>
+                <h3 className="flex-1 text-base lg:text-lg font-semibold">The.com Page Generation</h3>
+                <div className="flex flex-col gap-6 lg:gap-7 flex-1">
+                  <p className="text-base lg:text-lg text-[var(--text-secondary)]">Led product, design, and design systems for The.com’s no-code, sheet-based website builder and CMS tools.</p>
+                  <p className="text-base lg:text-lg text-[var(--text-secondary)]">I learned from early customers through generative and evaluative research, iterated and improved on the core product, and designed a brand new product surface area called Page Generation.</p>
                 </div>
               </div>
             </div>
