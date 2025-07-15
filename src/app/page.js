23b1d7e660d579e586031bc13b30438b13be0fe8
Matthew Traul <mclydep@gmail.com>
2025-07-15 14:59:46 -0600

    Add bg blur to header and adjust max-width syntax

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,10 @@ import { ModeToggle } from '../components/mode-toggle'
 
 export default function Home() {
   return (
-    <div className="px-6 sm:px-12 lg:px-20 pb-18 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col max-w-[70rem] mx-auto">
-        <header className="sticky top-0 py-6 bg-[var(--background)] z-10">
-          <div className="flex items-center justify-between w-full">
+    <div className="pb-18">
+      <main className="flex flex-col">
+        <header className="sticky top-0 py-4 px-6 sm:px-12 lg:px-20 bg-[var(--background)] bg-background/90 backdrop-blur supports-[backdrop-filter]:bg-background/90 z-10">
+          <div className="flex items-center justify-between w-full max-w-[70rem] mx-auto">
             <div className="flex items-center gap-3">
               <Image
                 src={Avatar}
@@ -23,94 +23,96 @@ export default function Home() {
             <ModeToggle />
           </div>
         </header>
-        <article className="flex flex-col gap-16 mt-16 lg:gap-32 lg:mt-40">
-          <section className="flex flex-col gap-8 lg:gap-14">
-            <h1 className="text-2xl-small font-semibold tracking-tight lg:text-2xl">Product designer and design engineer specializing in designer, developer, and collaboration tools</h1>
-            <p className="text-lg text-[var(--text-secondary)] max-w-[64rem] lg:text-xl">12+ years of experience designing 0 → 1 products, shipping software at scale, and building design systems.</p>
-          </section>
-          <section className="flex flex-col">
-            <h2 className="text-lg font-semibold tracking-tight lg:text-xl">Selected Work</h2>
-            <div className="flex flex-col gap-10 lg:gap-22">
-              <ProjectCard
-                videoSrc="/images/BubbleNativeMobileTabBar.mp4"
-                imageSrc="/images/TheDotCom.png"
-                imageAlt="Bubble Native Mobile Apps"
-                title="Bubble Native Mobile Apps"
-                logoSrc="/images/LogoBubble.svg"
-                paragraphs={[
-                  "Drove the direction, led the design, and executed on the details for the launch of the largest product expansion in Bubble's history.",
-                  "I designed the core concepts for how to design, build, preview, and publish native mobile apps for iOS and Android in a redesigned no-code editor."
-                ]}
-              />
-              <ProjectCard
-                imageSrc="/images/TheDotCom.png"
-                imageAlt="Bubble Editor UI"
-                title="Bubble Editor UI"
-                logoSrc="/images/LogoBubble.svg"
-                paragraphs={[
-                  "Placeholder"
-                ]}
-              />
-              <ProjectCard
-                imageSrc="/images/TheDotCom.png"
-                imageAlt="Bubble Pop Design System"
-                title="Bubble Pop Design System"
-                logoSrc="/images/LogoBubble.svg"
-                paragraphs={[
-                  "Placeholder"
-                ]}
-              />
-              <ProjectCard
-                videoSrc="/images/TheDotCom.mp4"
-                imageSrc="/images/TheDotCom.png"
-                imageAlt="The.com Page Generation"
-                title="The.com Page Generation"
-                logoSrc="/images/LogoTheDotCom.svg"
-                paragraphs={[
-                  "Led product, design, and design systems for The.com's no-code, sheet-based website builder and CMS tools.",
-                  "I learned from early customers through generative and evaluative research, iterated and improved on the core product, and designed a brand new product surface area called Page Generation."
-                ]}
-              />
-              <ProjectCard
-                imageSrc="/images/TheDotCom.png"
-                imageAlt="The.com CSS Sheet Groups"
-                title="The.com CSS Sheet Groups"
-                logoSrc="/images/LogoTheDotCom.svg"
-                paragraphs={[
-                  "Placeholder"
-                ]}
-              />
-              <ProjectCard
-                imageSrc="/images/TheDotCom.png"
-                imageAlt="QA Wolf Web App"
-                title="QA Wolf Web App"
-                logoSrc="/images/LogoQAWolf.svg"
-                paragraphs={[
-                  "Placeholder"
-                ]}
-              />
-              <ProjectCard
-                videoSrc="/images/InVisionInspectMotion.mp4"
-                imageSrc="/images/TheDotCom.png"
-                imageAlt="InVision Inspect Motion"
-                title="InVision Inspect Motion"
-                logoSrc="/images/LogoInvision.svg"
-                paragraphs={[
-                  "Placeholder"
-                ]}
-              />
-              <ProjectCard
-                videoSrc="/images/InVisionStudioJiraIntegration.mp4"
-                imageSrc="/images/TheDotCom.png"
-                imageAlt="InVision Studio Jira Integration"
-                title="InVision Studio Jira Integration"
-                logoSrc="/images/LogoInvision.svg"
-                paragraphs={[
-                  "Placeholder"
-                ]}
-              />
-            </div>
-          </section>
+        <article className="flex flex-col gap-16 px-6 mt-16 sm:px-12 lg:px-20 lg:gap-32 lg:mt-40">
+          <div className="flex flex-col max-w-[70rem] mx-auto">
+            <section className="flex flex-col gap-8 lg:gap-14">
+              <h1 className="text-2xl-small font-semibold tracking-tight lg:text-2xl">Product designer and design engineer specializing in designer, developer, and collaboration tools</h1>
+              <p className="text-lg text-[var(--text-secondary)] max-w-[64rem] lg:text-xl">12+ years of experience designing 0 → 1 products, shipping software at scale, and building design systems.</p>
+            </section>
+            <section className="flex flex-col">
+              <h2 className="text-lg font-semibold tracking-tight lg:text-xl">Selected Work</h2>
+              <div className="flex flex-col gap-10 lg:gap-22">
+                <ProjectCard
+                  videoSrc="/images/BubbleNativeMobileTabBar.mp4"
+                  imageSrc="/images/TheDotCom.png"
+                  imageAlt="Bubble Native Mobile Apps"
+                  title="Bubble Native Mobile Apps"
+                  logoSrc="/images/LogoBubble.svg"
+                  paragraphs={[
+                    "Drove the direction, led the design, and executed on the details for the launch of the largest product expansion in Bubble's history.",
+                    "I designed the core concepts for how to design, build, preview, and publish native mobile apps for iOS and Android in a redesigned no-code editor."
+                  ]}
+                />
+                <ProjectCard
+                  imageSrc="/images/TheDotCom.png"
+                  imageAlt="Bubble Editor UI"
+                  title="Bubble Editor UI"
+                  logoSrc="/images/LogoBubble.svg"
+                  paragraphs={[
+                    "Placeholder"
+                  ]}
+                />
+                <ProjectCard
+                  imageSrc="/images/TheDotCom.png"
+                  imageAlt="Bubble Pop Design System"
+                  title="Bubble Pop Design System"
+                  logoSrc="/images/LogoBubble.svg"
+                  paragraphs={[
+                    "Placeholder"
+                  ]}
+                />
+                <ProjectCard
+                  videoSrc="/images/TheDotCom.mp4"
+                  imageSrc="/images/TheDotCom.png"
+                  imageAlt="The.com Page Generation"
+                  title="The.com Page Generation"
+                  logoSrc="/images/LogoTheDotCom.svg"
+                  paragraphs={[
+                    "Led product, design, and design systems for The.com's no-code, sheet-based website builder and CMS tools.",
+                    "I learned from early customers through generative and evaluative research, iterated and improved on the core product, and designed a brand new product surface area called Page Generation."
+                  ]}
+                />
+                <ProjectCard
+                  imageSrc="/images/TheDotCom.png"
+                  imageAlt="The.com CSS Sheet Groups"
+                  title="The.com CSS Sheet Groups"
+                  logoSrc="/images/LogoTheDotCom.svg"
+                  paragraphs={[
+                    "Placeholder"
+                  ]}
+                />
+                <ProjectCard
+                  imageSrc="/images/TheDotCom.png"
+                  imageAlt="QA Wolf Web App"
+                  title="QA Wolf Web App"
+                  logoSrc="/images/LogoQAWolf.svg"
+                  paragraphs={[
+                    "Placeholder"
+                  ]}
+                />
+                <ProjectCard
+                  videoSrc="/images/InVisionInspectMotion.mp4"
+                  imageSrc="/images/TheDotCom.png"
+                  imageAlt="InVision Inspect Motion"
+                  title="InVision Inspect Motion"
+                  logoSrc="/images/LogoInvision.svg"
+                  paragraphs={[
+                    "Placeholder"
+                  ]}
+                />
+                <ProjectCard
+                  videoSrc="/images/InVisionStudioJiraIntegration.mp4"
+                  imageSrc="/images/TheDotCom.png"
+                  imageAlt="InVision Studio Jira Integration"
+                  title="InVision Studio Jira Integration"
+                  logoSrc="/images/LogoInvision.svg"
+                  paragraphs={[
+                    "Placeholder"
+                  ]}
+                />
+              </div>
+            </section>
+          </div>
         </article>
       </main>
       <footer className="flex gap-6 flex-wrap items-center justify-center">
