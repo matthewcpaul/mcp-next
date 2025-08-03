3ee0cd1a2b63fb90a65e9c0404364e25f3338f19
Matthew Traul <mclydep@gmail.com>
2025-08-03 13:23:50 -0600

    Added testimonials

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,10 @@ import { GeistMono } from 'geist/font/mono';
 import { ModeToggle } from '../components/mode-toggle'
 import Image from 'next/image';
 import Avatar from '../../public/images/avatar.png'
+import TestimonialChristine from '../../public/images/TestimonialChristine.jpeg'
+import TestimonialNick from '../../public/images/TestimonialNick.jpeg'
+import TestimonialSteven from '../../public/images/TestimonialSteven.jpeg'
+import TestimonialZack from '../../public/images/TestimonialZack.jpeg'
 import LogoToolshed from '../../public/images/LogoToolshed.svg'
 import LogoBubble from '../../public/images/LogoBubble.svg'
 import LogoTheDotCom from '../../public/images/LogoTheDotCom.svg'
@@ -122,10 +126,96 @@ export default function Home() {
             </section>
             <section className="flex flex-col gap-6 lg:gap-7">
               <div className={GeistMono.className}>
+                <h2 className="text-sm font-medium tracking-wider uppercase">Testimonials</h2>
+              </div>
+
+
+              <div className="grid lg:grid-cols-2 lg:gap-x-20">
+
+
+                <div className="flex gap-3 items-start border-t pt-4 pb-6 lg:pt-6 lg:pb-8">
+                  <Image
+                    src={TestimonialChristine}
+                    alt="Avatar"
+                    width={32}
+                    height={32}
+                    className="rounded-full w-7 lg:w-8"
+                    unoptimized
+                  />
+                  <div className="">
+                    <p className="text-base lg:text-lg/8">Christine Shiba</p>
+                    <div className={GeistMono.className}>
+                      <p className="mb-3 text-base text-[var(--text-secondary)] lg:text-[17px]">Product Designer &#183; Bubble</p>
+                    </div>
+                    <p className="text-balance text-base text-[var(--text-secondary)] lg:text-lg">Matthew is one of the best designers I’ve ever worked with. He is next level when it comes to design systems, interaction design, and pixel perfect components. He clearly cares deeply about his craft and transformed Bubble’s design quality threshold by shipping thoughtful designs and advocating across stakeholders. I really enjoyed my time working with him!</p>
+                  </div>
+                </div>
+
+                <div className="flex gap-3 items-start border-t pt-4 pb-6 lg:pt-6 lg:pb-8">
+                  <Image
+                    src={TestimonialNick}
+                    alt="Avatar"
+                    width={32}
+                    height={32}
+                    className="rounded-full w-7 lg:w-8"
+                    unoptimized
+                  />
+                  <div className="">
+                    <p className="text-base lg:text-lg/8">Nicolas Carroll</p>
+                    <div className={GeistMono.className}>
+                      <p className="mb-3 text-base text-[var(--text-secondary)] lg:text-[17px]">Product Manager &#183; Bubble</p>
+                    </div>
+                    <p className="text-balance text-base text-[var(--text-secondary)] lg:text-lg">Matthew and I worked together as founding members of Bubble’s Mobile team. I was really impressed with how quickly he was able to ramp up on Bubble’s product and bring his domain expertise of mobile development and design patterns to craft the vision for the mobile apps editor. He was a strong collaborator with engineering as well, working closely from project kickoff through design QA to ensure successful releases. I can say with confidence that the mobile product offering would not be where it’s at today without his design leadership.</p>
+                  </div>
+                </div>
+
+                <div className="flex gap-3 items-start border-t lg:border-b pt-4 pb-6 lg:pt-6 lg:pb-8">
+                  <Image
+                    src={TestimonialZack}
+                    alt="Avatar"
+                    width={32}
+                    height={32}
+                    className="rounded-full w-7 lg:w-8"
+                    unoptimized
+                  />
+                  <div className="">
+                    <p className="text-base lg:text-lg/8">Zack Tanner</p>
+                    <div className={GeistMono.className}>
+                      <p className="mb-3 text-base text-[var(--text-secondary)] lg:text-[17px]">Engineering Manager &#183; Vercel</p>
+                    </div>
+                    <p className="text-balance text-base text-[var(--text-secondary)] lg:text-lg">Matthew is a very passionate product designer. As an engineer working with Matthew, it was refreshing to see the amount of care and thought he put into his designs. He genuinely cares about and understands users of the products he builds, and his closeness to developer communities resulted in product designs & features that added value and were well-received within the frontend communities.</p>
+                  </div>
+                </div>
+
+                <div className="flex gap-3 items-start border-t border-b pt-4 pb-6 lg:pt-6 lg:pb-8">
+                  <Image
+                    src={TestimonialSteven}
+                    alt="Avatar"
+                    width={32}
+                    height={32}
+                    className="rounded-full w-7 lg:w-8"
+                    unoptimized
+                  />
+                  <div className="">
+                    <p className="text-base lg:text-lg/8">Steven Fabre</p>
+                    <div className={GeistMono.className}>
+                      <p className="mb-3 text-base text-[var(--text-secondary)] lg:text-[17px]">CEO &#183; Liveblocks</p>
+                    </div>
+                    <p className="text-balance text-base text-[var(--text-secondary)] lg:text-lg">I worked closely with Matthew for a good portion of his time at InVision, and we also worked together on a project for a couple of months. In that time, I’ve seen him contribute to the InVision design system like no others. He also has a good understanding of the technical side of things, which really pays off when it comes to executing an idea with engineers. Matthew is also a good listener, and genuinely cares about improving his skills. It’s rare to find someone willing to use feedback to drive real change and personal growth.</p>
+                  </div>
+                </div>
+
+
+              </div>
+
+
+            </section>
+            <section className="flex flex-col gap-6 lg:gap-7">
+              <div className={GeistMono.className}>
                 <h2 className="text-sm font-medium tracking-wider uppercase">Work Experience</h2>
               </div>
               <div className="flex flex-col">
-                <div className="flex items-center gap-4 py-4 border-t dark:border-border lg:py-6">
+                <div className="flex items-center gap-4 py-4 border-t lg:py-6">
                   <div className="flex gap-3 items-center flex-grow">
                     <Image
                       src={LogoToolshed}
@@ -142,7 +232,7 @@ export default function Home() {
                     <span className="text-base text-[var(--text-secondary)] lg:text-[17px]">2025—&nbsp;&nbsp;</span>
                   </div>
                 </div>
-                <div className="flex items-center gap-4 py-4 border-t dark:border-border lg:py-6">
+                <div className="flex items-center gap-4 py-4 border-t lg:py-6">
                   <div className="flex gap-3 items-center flex-grow">
                     <Image
                       src={LogoBubble}
@@ -159,7 +249,7 @@ export default function Home() {
                     <span className="text-base text-[var(--text-secondary)] lg:text-[17px]">2023—24</span>
                   </div>
                 </div>
-                <div className="flex items-center gap-4 py-4 border-t dark:border-border lg:py-6">
+                <div className="flex items-center gap-4 py-4 border-t lg:py-6">
                   <div className="flex gap-3 items-center flex-grow">
                     <Image
                       src={LogoTheDotCom}
@@ -176,7 +266,7 @@ export default function Home() {
                     <span className="text-base text-[var(--text-secondary)] lg:text-[17px]">2022—23</span>
                   </div>
                 </div>
-                <div className="flex items-center gap-4 py-4 border-t dark:border-border lg:py-6">
+                <div className="flex items-center gap-4 py-4 border-t lg:py-6">
                   <div className="flex gap-3 items-center flex-grow">
                     <Image
                       src={LogoQAWolf}
@@ -193,7 +283,7 @@ export default function Home() {
                     <span className="text-base text-[var(--text-secondary)] lg:text-[17px]">2020—21</span>
                   </div>
                 </div>
-                <div className="flex items-center gap-4 py-4 border-t dark:border-border lg:py-6">
+                <div className="flex items-center gap-4 py-4 border-t lg:py-6">
                   <div className="flex gap-3 items-center flex-grow">
                     <Image
                       src={LogoInvision}
@@ -210,7 +300,7 @@ export default function Home() {
                     <span className="text-base text-[var(--text-secondary)] lg:text-[17px]">2018—20</span>
                   </div>
                 </div>
-                <div className="flex items-center gap-4 py-4 border-t border-b dark:border-border lg:py-6">
+                <div className="flex items-center gap-4 py-4 border-t border-b lg:py-6">
                   <div className="flex gap-3 items-center flex-grow">
                     <Image
                       src={LogoIBM}
