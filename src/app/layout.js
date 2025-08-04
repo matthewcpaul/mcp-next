b43e1ee14cf952601afe0ab5846e2eb8f471d69d
Matthew Traul <mclydep@gmail.com>
2025-08-03 19:46:04 -0600

    Test metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,24 @@ import { Analytics } from '@vercel/analytics/next'
 
 export const metadata = {
   title: "Matthew Paul",
-  description: "Software Designer specializing in designer, developer, and collaboartion tools",
+  description: "Software product designer specializing in designer, developer, and collaboartion tools",
+  keywords: ['Matthew Paul', 'Product Designer', 'Designer', 'Software Designer', 'Design Systems', 'Design Engineer'],
+  creator: 'Matthew Paul',
+  openGraph: {
+    title: 'Matthew Paul',
+    description: 'Software product designer specializing in designer, developer, and collaboartion tools',
+    url: 'https://matthewcpaul.com',
+    siteName: 'Matthew Paul',
+    images: [
+      {
+        url: 'https://matthewcpaul.com/og/social-image.jpg', // Must be an absolute URL
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
